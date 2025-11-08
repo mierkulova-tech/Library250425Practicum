ca3911a5b9b11de678127c59f9d470b0a1431606
unknown <ps4spielevladi@gmail.com>
2025-11-08 15:37:00 +0300

    Add Rating for Authors

--- library/models.py
+++ library/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Author(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=50)
     birthday = models.DateField()
+    profile = models.URLField(blank=True, null=True)
+    deleted = models.BooleanField(default=False)
+    rating = models.FloatField(
+        validators=[MinValueValidator(1),
+                    MaxValueValidator(10)],
+        default=0
+    )
 
     def __str__(self):
         return f"{self.last_name[0]}. {self.first_name}"
